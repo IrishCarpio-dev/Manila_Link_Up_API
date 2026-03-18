0a8779dad5d5dbe9541cac464e65a1fc9dd4b1bd
IrishCarpio-dev <preciousirishc@gmail.com>
2026-03-18 10:47:33 +0800

    firebase database entry without auth

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use Kreait\Firebase\Contract\Database;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\SeekerController;
 
 // This is your test route
 Route::get('/test-firebase', function (Database $database) {
@@ -15,3 +17,26 @@ Route::get('/test-firebase', function (Database $database) {
 });
 
 Route::post('/register-seeker', [AuthController::class, 'registerSeeker']);
+
+// Route::middleware(['firebase.auth'])->group(function () {
+//     Route::get('/dashboard', function () {
+//         $type = $request->firebase_user_type;
+//         $data = $request->firebase_data;
+
+//         return response()->json([
+//             'user_type' => $type,
+//             'dashboard' => match ($type) {
+//                 'admins' => 'Admin Dashboard',
+//                 'users' => 'User Dashboard',
+//                 'enterprises' => 'Enterprise Dashboard',
+//                 default => 'Unknown Dashboard'
+//             },
+//             'user' => $data
+//         ]);
+//     });
+
+//     Route::post('/signup/seeker', [SeekerController::class, 'signup']);
+// });
+
+
+    Route::post('/signup/seeker', [SeekerController::class, 'signup']);
